c74b6c81fed7e9a9af9d9162de5850ad7df77680
Tearar <mps.adlhoch@gmail.com>
2023-12-24 14:57:04 +0100

    first commit

---
+++ backend/src/main/java/de/ma/volleyball/ToolSuite/ToolSuiteApplication.java
@@ -0,0 +1,13 @@
+package de.ma.volleyball.ToolSuite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolSuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolSuiteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/ma/volleyball/ToolSuite/ToolSuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ma.volleyball.ToolSuite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolSuiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
